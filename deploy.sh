3eb4d5f578c4682ba66984c49c20ed51ea2fa06e
Tema-ilit <artem_eliseev_2015@mail.ru>
2024-03-06 14:57:00 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#иницилизация репозитория и загрузка кода в GitHub
+git init
+git remote add origin https://github.com/Tema-ilit/Vue-Moire.git
+git add .
+git commit -m 'deploy'
+git push origin master
