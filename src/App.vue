bd953df8392807bc6d07672492d0f0887301fafb
Tema-ilit <artem_eliseev_2015@mail.ru>
2024-03-06 16:53:34 +0300

    version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,117 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="header container">
+    <div class="header__wrapper">
+      <span class="header__info">Каталог</span>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <router-link :to="{ name: 'home' }" class="header__logo">
+        <img
+          src="./assets/img/svg/logo-moire.svg"
+          alt="Логотип интернет магазина Moire"
+          width="116"
+          height="34"
+        />
+      </router-link>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <a class="header__tel" href="tel:8 800 600 90 09"> 8 800 600 90 09 </a>
+
+      <router-link
+        :to="{ name: 'basket' }"
+        class="header__cart"
+        href="cart.html"
+        aria-label="Корзина с товарами"
+      >
+        <svg width="19" height="24">
+          <use xlink:href="#icon-cart" />
+        </svg>
+        <span class="header__count" aria-label="Количество товаров">3</span>
+      </router-link>
     </div>
   </header>
 
   <RouterView />
+
+  <footer class="footer container">
+    <div class="footer__wrapper">
+      <ul class="footer__links">
+        <li>
+          <a class="footer__link" href="#"> Каталог </a>
+        </li>
+        <li>
+          <a class="footer__link" href="tel:88006009009"> 8 800 600 90 09 </a>
+        </li>
+        <li>
+          <a class="footer__link" href="mailto:hi@technozavrrr.com"> hi@technozavrrr.com </a>
+        </li>
+        <li>
+          <a class="footer__link" href="#"> Распродажа </a>
+        </li>
+        <li>
+          <a class="footer__link footer__link--medium" href="#"> Заказать звонок </a>
+        </li>
+      </ul>
+
+      <ul class="footer__social social">
+        <li class="social__item">
+          <a class="social__link" href="#" aria-label="Вконтакте">
+            <svg width="20" height="11" fill="currentColor">
+              <use xlink:href="#icon-vk"></use>
+            </svg>
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#" aria-label="Инстаграм">
+            <svg width="17" height="17" fill="currentColor">
+              <use xlink:href="#icon-insta"></use>
+            </svg>
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#" aria-label="Facebook">
+            <svg width="17" height="17" fill="currentColor">
+              <use xlink:href="#icon-facebook"></use>
+            </svg>
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#" aria-label="Twitter">
+            <svg width="17" height="14" fill="currentColor">
+              <use xlink:href="#icon-twitter"></use>
+            </svg>
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="#" aria-label="Telegram">
+            <svg width="19" height="17" fill="currentColor">
+              <use xlink:href="#icon-telegram"></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+
+      <p class="footer__desc">
+        Все права на&nbsp;материалы, находящиеся на&nbsp;сайте, охраняются в&nbsp;соответствии
+        с&nbsp;законодательством&nbsp;РФ, в&nbsp;том числе об&nbsp;авторском праве и&nbsp;смежных
+        правах.
+      </p>
+
+      <ul class="footer__data">
+        <li>
+          <a class="footer__link" href="#" target="_blank" rel="noopener" type="application/pdf">
+            Политика конфиденциальности
+          </a>
+        </li>
+        <li>
+          <a class="footer__link" href="#" target="_blank" rel="noopener" type="application/pdf">
+            Публичная оферта
+          </a>
+        </li>
+      </ul>
+
+      <span class="footer__copyright"> © 2020 Moire </span>
+    </div>
+  </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/style.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
